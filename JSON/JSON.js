9c7216272492ea298bab218b0949586db1b67181
Sandip Chitale <sandipchitale@gmail.com>
2013-03-26 11:15:46 -0700

    editobject directive.

--- JSON/JSON.js
+++ JSON/JSON.js
@@ -1,10 +1,43 @@
-angular.module('JSON', []).directive("editproperty", function() {
+angular.module('JSON', []).directive("editobject", function() {
     return {
         restrict : 'E',
         scope : {
             object : '='
         },
-       templateUrl: 'JSON.html',
+        templateUrl: 'editobject.html',
+//        template :
+//'<pre>{<br/><span ng-repeat="(p,v) in object"><nobr>&nbsp;&nbsp;<button ng-click="removeProperty(p)" title="Remove"> - </button>{{comma($index)}}\'{{p}}\' : {{valueToSet(v)}}</nobr><br></span>}</pre>',
+        controller : function($scope) {
+            $scope.removeProperty = function(p) {
+                delete $scope.object[p];
+                $scope.propertyName = '';
+            };
+            
+            $scope.valueToSet = function(value) {
+                var f = parseFloat(value);
+                if (f !== NaN && f == value) {
+                    return f;
+                } else {
+                    var i = parseInt(value);
+                    if (i !== NaN && i == value) {
+                        return i;
+                    }
+                }
+                return "'" + value + "'";
+            }
+            
+            $scope.comma = function(index) {
+                return (index > 0 ? ',' : ' ');
+            } 
+        }
+    }
+}).directive("editproperty", function() {
+    return {
+        restrict : 'E',
+        scope : {
+            object : '='
+        },
+        templateUrl: 'editproperty.html',
 //        template :
 //'<div> <input type="text" ng-model="propertyName" placeholder="property name"/><select style="width:1.5em;" ng-model="propertyName" ng-options="p as p for (p,v) in object"></select><span> : </span> <input type="text" ng-model="propertyValue" placeholder="value"/> <input type="checkbox" ng-model="overwrite" title="Overwrite"> <button ng-disabled="addUpdateDisabled" ng-click="addProperty()" title="{{operationTitle}}"> {{operation}} </button> <button ng-visible="removeVisible" ng-click="removeProperty()" title="Remove"> - </button> </div>',
         controller : function($scope) {
